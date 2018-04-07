2e56edc7f3169ec3891765d7e75cc0e0fd16c3a8
sreekanthkarakula <k.sreekanthreddy5a2@gmail.com>
2018-04-06 17:48:11 -0700

    count char solved

--- src/com/sree/java_puzzles/CountChars.java
+++ src/com/sree/java_puzzles/CountChars.java
@@ -6,6 +6,44 @@ public class CountChars {
 	
 	public static void main(String args[]){
 		
+		String input =  "aabcedddggj";
+		
+		int count=0, index =0;
+		
+		int length = input.length();
+		
+		char check='\0';
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(index<length) {
+			
+			if(index == 0) {
+				
+				count++;
+			    check = input.charAt(0);
+				index++;
+			
+			}
+		    if(check == input.charAt(index)) {
+				
+				count++;
+			    check = input.charAt(index);
+				index++;
+			}
+			
+			else {
+				sb.append(count);
+				sb.append(check);
+				check = input.charAt(index);
+				count=1;
+				index++;
+			}
+			
+		}
+		
+		System.out.println("this is o/p  " + sb);
+		
 		
 	}
 
